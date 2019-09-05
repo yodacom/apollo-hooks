a0549dd50b6c9d2e5df772ea10a46a0a58e1a204
Jeremy Black <jb@yodacom.com>
2019-09-04 18:07:27 -0600

    building block for apollo v2 with hooks

--- src/App.js
+++ src/App.js
@@ -2,25 +2,61 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import gql from 'graphql-tag'
+import { useQuery useMutation } from '@apollo/react-hooks'
+
+// create a variable called mutation to place our graphql mutation list, use createTacos as an example
+const mutation = gql`
+  mutation createTaco($name: String! $description: String) {
+    createTaco(name: $name, description: $description) {
+      id name description
+    }
+  }
+`:
+// create a variable called query to place our graphql query list, use listTacos as an example
+const query = gql`
+  query listTacos {
+    items {
+      id name description
+    }
+  }
+`;
+
+
 function App() {
+  //need a place to hold the values created from our Form Input using useState Hook. Here as example we are using one for name and one for description
+  const [name, setName] = useState('')
+  const [description, setDescription] = useState('')
+  
+  // useQuery hook passing in the query to return the data in the loading state from the query
+  const { loading, data } = useQuery(query)
+
+  // useMutation hook - the return value here is a function called createTaco and a check for errors with error. The first argument is our mutation definition "mutation" The second argument to pass in are the variables we are passing into the mutation: name and description. The third argument to useMutation is the refetch queries that we would like to call after the mutation has been successful: listTacos
+  const [createTaco, { error }] = useMutation(mutation, {
+    variable: { name, description }, refetchQueries: ["listTacos"]
+  })
+  if (error) {
+    console.log("error":, error)
+  }
+  if (loading) return <h2>Loading...</h2>
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input onChange={e => setName(e.target.value)}
+      />
+      <input onChange={e => setDescription(e.target.value)}
+      />
+      <button onClick={createTaco}>Create Taco</button>
+      {/* map over the items array returned from the query returning the item name and the item description */}
+      {
+        data.listTacos.items.map((item, index) => {
+          <div key={item.id}>
+            <h2>{item.name</h2>
+            <h4>{item.description</h4>
+  
+        </div>
+      })}
     </div>
   );
 }
-
+ 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,7 +4,39 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { ApolloClient } from 'apollo-client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { setContext } from 'apollo-link-context';
+import { createHttpLink } from 'apollo-link-http'
+import { ApolloProvider } from '@apollo/react-hooks'
+
+// create http link to our graphql api 
+const httpLink = createHttpLink({
+  uri: '<HTTPS://YOUR GRAPHQL API LINK>'
+})
+// if you need to send headers to your graphql endpoint set them in authLink and set your header properties
+const authLink = setContext({ _, { headers }} => {
+  return {
+    headers: {
+      'x-api-key': " <YOUR API KEY> "
+    }
+  }
+})
+//Create client variable by calling new Apollo Client and passing in the the link and the cache property as new end memory cache.  If you do not have an auth link you can just pass in the httpLink as the link property
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache()
+})
+
+// create an Apollo app component passing in the client as client into the Apollo Provider
+const ApolloApp = () => (
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
+)
+
+// in ReactDom render replace the  <App /> component with our new ApolloApp component
+ReactDOM.render(<ApolloApp />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
